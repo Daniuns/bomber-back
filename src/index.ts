d697181c029b3a23c20c93dea57aa54841c64243
daniel <daniel.ferreira@eduzz.com>
2020-09-21 10:18:37 -0300

    remove socket to test application

--- src/index.ts
+++ src/index.ts
@@ -14,34 +14,32 @@ const server = app.listen(8080, () => {
     console.log(`server started: PORT: 8080`);
 });
 
-const io = require("socket.io")(server, {origin: '*:*'});
+// const io = require("socket.io")(server, {origin: '*:*'});
     // io.origins(['https://app-bomber.herokuapp.com:3000']);
 
     app.get('/test', function(req: any, res:any) {
         res.send('Everything Ok!');
       });
 
-
-
     process.on('unhandledRejection', () => { /* ignore */ });
 
 
-io.on('connection', (socket: any) => {
-    console.log('new user connected.', socket.id);
+// io.on('connection', (socket: any) => {
+//     console.log('new user connected.', socket.id);
 
-    io.sockets.emit('receiveMap', mapService.generateMap());
-    io.sockets.emit('createHero', heroesService.createHero(socket.id));
-    io.sockets.emit('heroesList', heroesService.getHeroes());
-    socket.on('attHero', async (hero: IHero) => {
-        console.log('atualiza esse hero', hero);
-        await heroesService.setHero(hero);
-        io.sockets.emit('heroesList', heroesService.getHeroes());
-    });
-    // io.sockets.emit('enteredAt', new Date().getTime());
+//     io.sockets.emit('receiveMap', mapService.generateMap());
+//     io.sockets.emit('createHero', heroesService.createHero(socket.id));
+//     io.sockets.emit('heroesList', heroesService.getHeroes());
+//     socket.on('attHero', async (hero: IHero) => {
+//         console.log('atualiza esse hero', hero);
+//         await heroesService.setHero(hero);
+//         io.sockets.emit('heroesList', heroesService.getHeroes());
+//     });
+//     // io.sockets.emit('enteredAt', new Date().getTime());
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected.', socket.id);
-        heroesService.disconnected(socket.id);
-        io.sockets.emit('heroesList', heroesService.getHeroes());
-    });
-});
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected.', socket.id);
+//         heroesService.disconnected(socket.id);
+//         io.sockets.emit('heroesList', heroesService.getHeroes());
+//     });
+// });
